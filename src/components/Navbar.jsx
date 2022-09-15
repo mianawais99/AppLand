a566969d7cc6a2ebb8463b0f67b98ac451868d79
MIAN AWAIS <mianawais4832@hotmail.com>
2022-09-16 02:08:35 +0500

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import logo from '../images/logo.png';
 
 function Navbar() {
   return (
     <nav className={}>
-
+        <a href='#'>
+            <img src={logo} alt='AppLand' />
+        </a>
+        <input type='checkbox' className='menu-btn' id='menu-btn' />
+        <label className='menu-icon' for='menu-btn' />
     </nav>
   )
 }
